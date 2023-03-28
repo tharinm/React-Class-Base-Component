49e30be0b28f980c33886cf6e8010944055e8ad8
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-28 10:58:49 +0530

    error handled in input box

--- src/components/Homelayout/NewTaskinput.js
+++ src/components/Homelayout/NewTaskinput.js
@@ -3,21 +3,57 @@ import React, { Component } from "react";
 export default class NewTaskinput extends Component {
   state = {
     inputValue: "",
+    isError: false,
   };
 
   handleInputValueChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    const { value } = e.target;
+    if (value.trim) {
+      this.setState({
+        isError: false,
+      });
+    } else {
+      // this.setState({
+      // isError: true,
+      // });
+    }
     this.setState({
-      inputValue: e.target.value
-     })
+      inputValue: e.target.value,
+    });
+  };
+
+  handleAddNewTask = () => {
+    const { inputValue } = this.state;
+    if (inputValue.trim()) {
+      this.props.addNewTask(inputValue);
+      this.setState(
+        {
+          inputValue: ''
+        }
+      )
+    } else {
+     // console.log("second");
+      this.setState({
+        isError: true,
+      });
+    }
   };
 
   render() {
-    const { addNewTask } = this.props;
+    // const { addNewTask } = this.props;
     return (
-      <div style={{ display: "flex", gap: "15px" }}>
-        <input type={"text"} onChange={this.handleInputValueChange} />
-        <button onClick={() => addNewTask(this.state.inputValue)}>Add</button>
+      <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+        <input type={"text"} onChange={this.handleInputValueChange} value={this.state.inputValue} />
+        <button onClick={this.handleAddNewTask}>Add</button>
+        <br></br>
+        {this.state.isError ? (
+          <p style={{ fontSize: "15px", color: "red" }}>
+            Please Enter a valid task
+          </p>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
